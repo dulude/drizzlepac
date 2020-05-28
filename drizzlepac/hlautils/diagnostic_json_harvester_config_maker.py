090c57d3adfb4f85e73c76373c0460581f933781
Michael Dulude <dulude@stsci.edu>
2020-05-28 18:33:53 -0400

    drizzlepac/hlautils/diagnostic_json_harvester_config_maker.py: partial functionality achieved. Code now can now process json files properly so long as they do not contain astropy tables. Fix coming soon.

--- drizzlepac/hlautils/diagnostic_json_harvester_config_maker.py
+++ drizzlepac/hlautils/diagnostic_json_harvester_config_maker.py
@@ -13,6 +13,55 @@ import pdb
 # Local application imports
 import drizzlepac.hlautils.diagnostic_utils as du
 
+def convert_nested(dct):
+    # empty dict to store the result
+    result = dict()
+
+    # create an iterator of lists
+    # representing nested or hierarchial flow
+    lsts = ([*k.split("."), v] for k, v in dct.items())
+
+    # insert each list into the result
+    for lst in lsts:
+        insert(result, lst)
+    return result
+# ------------------------------------------------------------------------------------------------------------
+
+
+def flatten_dict(dd, separator='.', prefix=''):
+    """Recursive subroutine to flatten nested dictionaries down into a single-layer dictionary.
+    Borrowed from https://www.geeksforgeeks.org/python-convert-nested-dictionary-into-flattened-dictionary/
+
+    Parameters
+    ----------
+    dd : dict
+        dictionary to flatten
+
+    separator : str, optional
+        separator character used in constructing flattened dictionary key names from multiple recursive
+        elements. Default value is '.'
+
+    prefix : str, optional
+        flattened dictionary key prefix. Default value is an empty string ('').
+
+    Returns
+    -------
+    a version of input dictionary *dd* that has been flattened by one layer
+    """
+    # TODO: From Michele...Just FYI - there is a Python package, flatten-dict, which will both flatten
+    #  dict-like objects and unflatten those objects!
+    return {prefix + separator + k if prefix else k: v
+            for kk, vv in dd.items()
+            for k, v in flatten_dict(vv, separator, kk).items()
+            } if isinstance(dd, dict) else {prefix: dd}
+
+# ------------------------------------------------------------------------------------------------------------
+
+def insert(dct, lst):
+    for x in lst[:-2]:
+        dct[x] = dct = dct.get(x, dict())
+    dct.update({lst[-2]: lst[-1]})
+
 # ------------------------------------------------------------------------------------------------------------
 
 def make_config_file():
@@ -76,10 +125,26 @@ def process_json_filetype(json_filetype, output_json_dict):
         if "general information" not in output_json_dict.keys():
             output_json_dict['general information'] = collections.OrderedDict()
             for gi_item in json_data['general information'].keys():
-                output_json_dict['general information'][gi_item] = "Human-readable title"
+                output_json_dict['general information'][gi_item] = "Human-readable title placeholder"
+
+        # update output_json_dict with information about the specified json file type
+        # if json_filetype != "_svm_photometry.json": # TODO: REMOVE
+        #     return output_json_dict # TODO: REMOVE
+        output_json_dict[json_filetype] = collections.OrderedDict()
+        for section_name in json_data['data'].keys():
+            # flatten out nested dictionaries
+            flattened_data = flatten_dict(json_data['data'][section_name])
+            # replace all values with a placeholder tuple
+            for flat_key in flattened_data:
+                print("           ",flat_key,type(flattened_data[flat_key]))
+                flattened_data[flat_key] = ("Human-readable title placeholder", "units placeholder")
+            # renest flattened dictionary and update output_json_dict
+            output_json_dict[json_filetype][section_name] = convert_nested(flattened_data)
     else:
         print("     No {} files found!".format(json_filetype))
     return output_json_dict
+
+
 
 
 # ======================================================================================================================
